0500b1c02ac3d553d31211bad7d31165282a7cb3
Takethisworld <62119373+Takethisworld@users.noreply.github.com>
2021-07-24 17:41:33 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ColoredUserpic from './coloredUserpic';
+
+function App() {
+  return (
+    <div >
+
+<ColoredUserpic
+/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/coloredUserpic.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css'
+
+const ColoredUserpic = () => {
+    return (<div >
+        <div className="border-wrap">
+            <img className='img' src='https://media.macphun.com/img/uploads/customer/how-to/579/15531840725c93b5489d84e9.43781620.jpg?q=85&w=1340' />
+            </div>
+            </div>)
+}
+export default ColoredUserpic
